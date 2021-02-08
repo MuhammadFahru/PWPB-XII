0c1af004267bade1dbaf70b8f6fe31ace563a263
MuhammadFahru <fahrumuhammad.ozi@gmail.com>
2021-02-08 09:37:14 +0700

    Tugas Basic SQLite

---
+++ BasicSQLite/app/src/main/java/com/fahru/basicsqlite/UserRowItem.java
@@ -0,0 +1,14 @@
+package com.fahru.basicsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserRowItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_row_item);
+    }
+}
